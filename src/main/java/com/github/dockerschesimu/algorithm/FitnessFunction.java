c38054b0dd09a22f2988fde274c39bced79c2cda
ztwang <ztwnag@qq.com>
2018-01-30 15:06:06 +0800

    add new module ACA and refactor old project

--- src/main/java/com/github/dockerschesimu/algorithm/FitnessFunction.java
+++
@@ -1,8 +0,0 @@
-package com.github.dockerschesimu.algorithm;
-
-public interface FitnessFunction {
-
-	public double fitness(int[] taskList);
-	public boolean canBeParent(int[] taskList);
-	public void pfitness(int[] sche);
-}
